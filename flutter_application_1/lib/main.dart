195f7339ac290f61d71a1705d1a3ba68c2596283
Flex Alderson <phantoinang2001@gmail.com>
2022-03-04 20:44:58 +0700

    add share to facebook button

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,32 +1,35 @@
 import 'dart:convert';
- 
+
 import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 import 'package:http/http.dart' as http;
- 
+import 'package:flutter_share_me/flutter_share_me.dart';
+
 void main() {
-  runApp(LoginPage());
+  runApp(const LoginPage());
 }
- 
+
 class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
   @override
   _LoginPageState createState() => _LoginPageState();
- 
 }
- 
+
 class _LoginPageState extends State<LoginPage> {
   bool isLoggedIn = false;
   var profileData;
- 
+
   var facebookLogin = FacebookLogin();
- 
+
   void onLoginStatusChanged(bool isLoggedIn, {profileData}) {
     setState(() {
       this.isLoggedIn = isLoggedIn;
       this.profileData = profileData;
     });
   }
- 
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -45,10 +48,9 @@ class _LoginPageState extends State<LoginPage> {
       ),
     );
   }
- 
+
   void loginButtonClicked() async {
-    var facebookLoginResult =
-    await facebookLogin.logIn(['email']);
+    var facebookLoginResult = await facebookLogin.logIn(['email']);
     switch (facebookLoginResult.status) {
       case FacebookLoginStatus.error:
         onLoginStatusChanged(false);
@@ -57,17 +59,17 @@ class _LoginPageState extends State<LoginPage> {
         onLoginStatusChanged(false);
         break;
       case FacebookLoginStatus.loggedIn:
-        var graphResponse = await http.get(
-            Uri.parse('https://graph.facebook.com/v2.12/me?fields=name,first_name,last_name,email,picture.height(200)&access_token=${facebookLoginResult.accessToken.token}'));
- 
+        var graphResponse = await http.get(Uri.parse(
+            'https://graph.facebook.com/v2.12/me?fields=name,first_name,last_name,email,picture.height(200)&access_token=${facebookLoginResult.accessToken.token}'));
+
         var profile = json.decode(graphResponse.body);
-        print(profile.toString());
- 
+        debugPrint(profile.toString());
+
         onLoginStatusChanged(true, profileData: profile);
         break;
     }
   }
- 
+
   _displayUserData(profileData) {
     return Column(
       mainAxisSize: MainAxisSize.min,
@@ -98,25 +100,39 @@ class _LoginPageState extends State<LoginPage> {
             fontSize: 20.0,
           ),
         ),
-          ElevatedButton(
-            child: const Text("Logout"),
-            onPressed: () => facebookLogin.isLoggedIn
-                .then((isLoggedIn) => isLoggedIn ? _logout() : {}),
-          )
+        ElevatedButton(
+          onPressed: () => onButtonTap(
+            'Đổi cái này trong main.dart, ở dòng số 104',
+            'https://github.com/often127',
+          ),
+          child: const Text("Facebook share"),
+        ),
+        ElevatedButton(
+          child: const Text("Logout"),
+          onPressed: () => facebookLogin.isLoggedIn
+              .then((isLoggedIn) => isLoggedIn ? _logout() : {}),
+        )
       ],
     );
   }
- 
+
   _displayLoginButton() {
     return ElevatedButton(
       child: const Text("Login with Facebook"),
       onPressed: () => loginButtonClicked(),
     );
   }
- 
+
   _logout() async {
     await facebookLogin.logOut();
     onLoginStatusChanged(false);
-    print("Logged out");
+    debugPrint("Logged out");
+  }
+
+  Future<void> onButtonTap(String msg, String url) async {
+    String? response;
+    final FlutterShareMe flutterShareMe = FlutterShareMe();
+    response = await flutterShareMe.shareToFacebook(url: url, msg: msg);
+    debugPrint(response);
   }
 }
